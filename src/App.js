36452d1e4fcdc241e5290cbdc7c5038f579678bd
Anisa <aagermanova@gmail.com>
2020-06-25 18:21:43 -0700

    Страница списка

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
   return (
     <Layout>
       <Switch>
-        <Route path="auth" component={Auth}/>
-        <Route path="quiz-creator" component={QuizCreator}/>
-        <Route path="quiz/:id" component={Quiz}/>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/quiz-creator" component={QuizCreator}/>
+        <Route path="/quiz/:id" component={Quiz}/>
         <Route path="/" component={QuizList}/>
       </Switch>
     </Layout>
