8502536365212b0a5f4f8cee64c0eb925c8a659c
Anisa <aagermanova@gmail.com>
2020-07-03 15:45:40 -0700

    Страница теста. Часть 1

--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -1,6 +1,6 @@
 
 import axios from '../../axios/axios-quiz'
-import {FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZES_ERROR} from './actionTypes'
+import {FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZES_ERROR, FETCH_QUIZ_SUCCESS} from './actionTypes'
 export function fetchQuizes() {
   return async dispatch => {
     dispatch(fetchQuizesStart())
@@ -22,6 +22,30 @@ export function fetchQuizes() {
     }
   }
 }
+
+export function fetchQuizById(quizId) {
+  return async dispatch => {
+    dispatch(fetchQuizesStart())
+
+    try {
+      const response = await axios.get(`/quizes/${quizId}.json`)
+      const quiz = response.data
+
+      dispatch(fetchQuizSuccess(quiz))
+    } catch (e) {
+      dispatch(fetchQuizesError(e))
+    }
+  }
+}
+
+
+export function fetchQuizSuccess(quiz) {
+  return {
+    type: FETCH_QUIZ_SUCCESS,
+    quiz
+  }
+}
+
 export function fetchQuizesStart() {
   return {
     type: FETCH_QUIZES_START
